49569349da660dbd36dc4b78a64a95ec55404b23
Harry Hu <hoozh1123@gmail.com>
2019-10-16 11:32:22 +1100

    Initial commit

---
+++ javascript/script.js
@@ -0,0 +1,42 @@
+// Progress Bar
+
+$(function () {
+
+    var prog = $(".progress-bar");
+
+    console.log(prog);
+  
+    prog.eq(0).animate({width:"80%"}, 2500)
+  
+    prog.eq(1).animate({width:"70%"}, 2500)
+  
+    prog.eq(2).animate({width:"75%"}, 2500)
+
+    prog.eq(3).animate({width:"95%"}, 2500)
+    
+    prog.eq(4).animate({width:"90%"}, 2500)
+
+    prog.eq(5).animate({width:"85%"}, 2500)
+  
+  });
+
+
+  
+  // Example starter JavaScript for disabling form submissions if there are invalid fields
+  $(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  });
